6c0fdec936b5e9eb838275aa8be2e88e14ea6692
Jon Kasperan <jkaspera@gmail.com>
2026-04-30 14:10:21 -0500

    APP-0001 Wrapped max-w-xs with a feature flag as a demonstration of feature flags

--- nextjsapp/app/page.tsx
+++ nextjsapp/app/page.tsx
@@ -12,10 +12,15 @@ export default function Home() {
           height={20}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          {process.env.NEXT_PUBLIC_FEATURE_NEW_DASHBOARD === "true" ? (
+           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started with feature flags, edit the page.tsx file.
+           </h1> ) : (
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
+          )}
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
